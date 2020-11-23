cba0170eea3fea431049ec39ee16ae6f0dff6405
Mateusz Malenta <mateusz.malenta@gmail.com>
2020-11-23 22:18:02 +0000

    Properly read the commit time
    Works properly with different style names

--- spring/__init__.py
+++ spring/__init__.py
@@ -4,7 +4,14 @@ with open(".git/logs/HEAD") as gf:
   all_lines = gf.read().splitlines()
   last_commit = all_lines[-1]
 
-git_sha = last_commit.split()[1][:7]
-date = datetime.utcfromtimestamp(int(last_commit.split()[5])).strftime("%y%m%d")
+# Remove the commit message
+last_commit = last_commit.split("\t")[0]
 
-__version__ = "0.1.0+" + date + "." + git_sha
+git_sha = last_commit.split()[1][:7]
+# Read from the end - full name is unreliable as it may not use
+# just two words (it can be anything really)
+date = datetime.utcfromtimestamp(int(last_commit.split()[-2])).strftime("%y%m%d")
+
+base_version = "0.1.1"
+
+__version__ = base_version + "+" + date + "." + git_sha
