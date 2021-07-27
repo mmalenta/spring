d9b1e65a9e5119fe0d3eaab216a53595e7117c57
Mateusz Malenta <mateusz.malenta@gmail.com>
2021-07-27 20:30:55 +0100

    Print out a warning on known source with label 0
    Will log a warning message if a known source is classified with label 0. Changed the dictionary to now hold the name of the source instead of a bool value. Converted into a bool when comparisons required.

--- spring/spmodule/spcompute/frbidmodule.py
+++ spring/spmodule/spcompute/frbidmodule.py
@@ -166,7 +166,7 @@ class FrbidModule(ComputeModule):
 
     pred_end = perf_counter()
 
-    logger.info("Label %d with probability of %.4f", label, prob)
+    logger.debug("Label %d with probability of %.4f", label, prob)
 
     cand_metadata = self._data.metadata["cand_metadata"]
 
@@ -184,7 +184,7 @@ class FrbidModule(ComputeModule):
 
     # Send the data to clustering only if the candidate is labelled
     # as probable and is not a known source
-    if label > 0.0 and not cand_metadata["known"]:
+    if label > 0.0 and not bool(cand_metadata["known"]):
       message = {
         "dm": self._data.metadata["cand_metadata"]["dm"],
         "mjd": self._data.metadata["cand_metadata"]["mjd"],
@@ -218,6 +218,13 @@ class FrbidModule(ComputeModule):
     # If the candidate is not labelled as probable or is a known source
     # then send directly to archiving
     else:
+
+      # We need to know quickly if a known source is classified 
+      # with label 0
+      if bool(cand_metadata["known"]) and label < 1.0:
+        logger.warning("Known source %s classified with label 0!",
+                        cand_metadata["known"])
+
       message = {
         "cand_hash": cand_hash
       }
