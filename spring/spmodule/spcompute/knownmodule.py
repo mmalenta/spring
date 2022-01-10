8f330b3990536c4e54ed473c57b9e9c45a1622c0
Mateusz Malenta <mateusz.malenta@gmail.com>
2022-01-10 14:36:47 +0000

    Feat(known): separate list of allowed known sources
    There are some known sources that we might be interested in getting the filterbank files of. Introduce additional list with these sources that passes them through the known sources module.
    
    Introduces additional file for logging to make finding of these sources easier.

--- spring/spmodule/spcompute/knownmodule.py
+++ spring/spmodule/spcompute/knownmodule.py
@@ -62,6 +62,19 @@ class KnownModule(ComputeModule):
       self._thresh_dist = 1.5
       self._thresh_dm = 5.0
       self._known_pass_ratio = 0.005
+      self._allowed_known = True
+      self._allowed_known_file = "/config/allowed_known_sources.dat"
+            
+      try:
+        self._allowed_known_list = open(self._allowed_known_file, 'r').read().splitlines()
+      except:
+        logger.error("Cannot load allowed known sources file %s! "
+                      "Will use an empty list instead!", 
+                      self._allowed_known_file)
+        self._allowed_known_list = []
+      else:
+        logger.info("Loaded the allowed sources list with %d sources ",
+                      len(self._allowed_known_list))
     else:
       self._catalogue = config["catalogue"]
       self._thresh_dist = config["thresh_dist"]
@@ -72,8 +85,9 @@ class KnownModule(ComputeModule):
 
     if self._catalogue not in self._matcher.supported_catalogues:
 
-      logger.warning("Unsupported catalogue %s! \
-                      Will default to PSRCAT", self._catalogue)
+      logger.warning("Unsupported catalogue %s! "
+                      "Will default to PSRCAT", 
+                      self._catalogue)
       self._catalogue = "psrcat"
 
     self._matcher.load_catalogue(self._catalogue)
@@ -111,7 +125,9 @@ class KnownModule(ComputeModule):
     known_matches = self._matcher.find_matches(beam_position,
                                                 cand_metadata["dm"])
 
-    if (known_matches is not None):
+    if (known_matches is not None and 
+        not (self._allowed_known and 
+          (known_matches[0] in self._allowed_known_list))):
       
       # Save the known source information
       # Separate file per beam for now
@@ -140,6 +156,26 @@ class KnownModule(ComputeModule):
         return False
 
     else:
+
+      if (known_matches is not None):
+
+        logger.info("This candidate is an allowed known source."
+                    "It wil be processed further")
+        cand_metadata["known"] = known_matches[0]
+
+        # Save the known source information
+        # Separate file per beam for now
+        fil_metadata = self._data.metadata["fil_metadata"]
+        allowed_file = path.join(fil_metadata["full_dir"],
+                              'allowed_known_sources.dat')      
+
+        with open(allowed_file, 'a') as af:
+          af.write("%.10f\t%.4f\t%.4f\t%.2f\t%d\t%s\t%s\t%s\t%s\t%s\n" % 
+          (cand_metadata["mjd"], cand_metadata["dm"],
+          cand_metadata["width"], cand_metadata["snr"],
+          beam_metadata["beam_abs"], beam_metadata["beam_type"],
+          beam_metadata['beam_ra'], beam_metadata["beam_dec"],
+          fil_metadata["fil_file"], known_matches[0]))
 
       return None
 
