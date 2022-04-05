c9dc5ff8c2be4d5953a8a4f50919e77e5f3ecdb5
Mateusz Malenta <mateusz.malenta@gmail.com>
2022-04-05 21:49:42 +0100

    Style(version): updated the base version

--- spring/__init__.py
+++ spring/__init__.py
@@ -12,6 +12,6 @@ git_sha = last_commit.split()[1][:7]
 # just two words (it can be anything really)
 date = datetime.utcfromtimestamp(int(last_commit.split()[-2])).strftime("%y%m%d")
 
-base_version = "0.1.1"
+base_version = "0.4.0"
 
 __version__ = base_version + "+" + date + "." + git_sha
